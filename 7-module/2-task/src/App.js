aa436dde147cae1c0fc2adf47bd7872255d49257
P4eJIa <p4ejia@gmail.com>
2023-04-08 15:17:04 +0530

    Готовое решение задачи "Модальное окно"

--- 7-module/2-task/src/App.js
+++ 7-module/2-task/src/App.js
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState(null);
   
+  useEffect(() => {
+    function keyDown(event) {
+      if (event.code === "Escape") {
+        closeModal();
+      }
+    }
+    document.addEventListener('keydown', keyDown);
+
+    return ()=> {
+      document.removeEventListener('keydown', keyDown)
+    }
+  }, []);
+
   const ModalButton = {
     background: "white", 
     color: "black", 
@@ -30,7 +43,12 @@ function App() {
       <button 
         className="button" 
         style={ModalButton} 
-        onClick={() => openModal("Заголовок модального окна", "<b>тут содержится тело модального окна</b>")}
+        onClick={() => 
+          openModal(
+            "Заголовок модального окна", 
+            "<b>тут содержится тело модального окна</b>"
+          )
+        }
       >
         Нажми меня, чтобы открыть модальное окно
       </button>
@@ -39,13 +57,19 @@ function App() {
         <div className="modal__overlay" onClick={closeModal}></div>
         <div className="modal__inner">
           <div className="modal__header">
-            <button type="button" className="modal__close" onClick={closeModal}>
+            <button 
+              type="button" 
+              className="modal__close" 
+              onClick={closeModal}
+            >
               <img src="/icons/cross-icon.svg" alt="close-icon" />
             </button>
             <h3 className="modal__title">{title}</h3>
           </div>
-          <div className="modal__body" dangerouslySetInnerHTML={{ __html: body }}>
-          </div>
+          <div 
+            className="modal__body" 
+            dangerouslySetInnerHTML={{ __html: body }}
+          ></div>
         </div>
       </div>
     )}
