0acc8354d466fdd627253c7f6b97ba153cdd623e
P4eJIa <p4ejia@gmail.com>
2023-04-08 10:25:13 +0530

    добавил верстку Модального окна

--- 7-module/2-task/src/App.js
+++ 7-module/2-task/src/App.js
@@ -1,7 +1,23 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <div className="modal">
+        <div className="modal__overlay"></div>
+        <div className="modal__inner">
+          <div className="modal__header">
+            <button type="button" className="modal__close">
+              <img src="/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+            <h3 className="modal__title">
+              Вот сюда нужно добавлять заголовок
+            </h3>
+          </div>
+          <div className="modal__body">
+            А сюда нужно добавлять содержимое модального окна
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
