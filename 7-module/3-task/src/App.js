db93436bea13ec9f5f6c20940e416fd2242f3f82
P4eJIa <p4ejia@gmail.com>
2023-04-20 10:15:39 +1000

    Добавил стили, верстку начальную

--- 7-module/3-task/src/App.js
+++ 7-module/3-task/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{padding: '50px'}}>
+      <div className="slider">
+
+        <div className="slider__thumb" style={{left: '50%'}}>
+          <span className="slider__value">2</span>
+        </div>
+
+        <div className="slider__progress" style={{width: '50%'}}></div>
+
+        <div className="slider__steps">
+          <span></span>
+          <span></span>
+          <span className="slider__step-active"></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
     </div>
   );
 }
