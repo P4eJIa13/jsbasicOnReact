b7b5af91730705e8f2ce73295256745139893c06
P4eJIa <p4ejia@gmail.com>
2023-04-22 16:43:51 +1000

    Решение 3 задаыи 7 модуля

--- 7-module/3-task/src/App.js
+++ 7-module/3-task/src/App.js
@@ -1,76 +1,11 @@
-import { useState } from "react";
+import StepSlider from "./StepSlider";
 
 function App() {
-
-  let config = {
-    steps: 5,
-    value: 0
-  }
-
-  const [activeStep, setActiveStep] = useState('');
-  const element = document.querySelector('.slider');
-
-  const activeSlider = ((evt, i) => {
-    evt.preventDefault();
-    let left = evt.clientX - element.getBoundingClientRect().left;
-    let leftRelative = left / element.offsetWidth;
-    let segments = config.steps - 1;
-    let value = Math.round(leftRelative * segments);
-    let valuePercents = value / segments * 100;
-    let thumb = element.querySelector('.slider__thumb');
-    let progress = element.querySelector('.slider__progress');
-    element.querySelector('.slider__value').innerHTML = value;
-    thumb.style.left = (value === 0 ? 0 : `${valuePercents}%`);
-    progress.style.width = (value === 0 ? 0 : `${valuePercents}%`)
-    const sliderStep = evt.target.closest('slider__step');
-    setActiveStep(i);
-    const prevActiveStep = document.querySelector('.slider__step-active');
-    if (prevActiveStep) {
-      prevActiveStep.classList.remove('slider__step-active');
-    }
-    if (sliderStep) {
-      sliderStep.classList.add('slider__step-active')
-    }
-    const slideSelectEvent = new CustomEvent('slider-change', {
-      detail: config.value,
-      bubbles: true
-    });
-    document.documentElement.dispatchEvent(slideSelectEvent);
-  })
-
-  const getSliderSpan = config => {
-    let spanHTML = [];
-    for (let i = 0; i < config.steps; i++) {
-      spanHTML.push(
-        <span 
-          className={`${i === activeStep ? 'slider__step-active' : ''}`}
-          key={i}
-          onClick={(evt) => activeSlider(evt, i)}
-        >
-          {i}
-        </span>
-      )
-    }
-    return spanHTML;
-  }
-
   return (
     <div className="container" style={{padding: '50px'}}>
-      <div className="slider">
-
-        <div className="slider__thumb" style={{left: '{(100 / (config.steps - 1)) * config.value}'}}>
-          <span className="slider__value"></span>
-        </div>
-
-        <div className="slider__progress" style={{width: '{(100 / (config.steps - 1)) * config.value}'}}></div>
-
-        <div className="slider__steps">
-          {getSliderSpan(config)}
-        </div>
-      </div>
-
+      <StepSlider config={{ steps: 5, value: 0 }} />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ 7-module/3-task/src/StepSlider.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function StepSlider({ config }) {
+  const [value, setValue] = useState(config.value);
+  const sliderRef = useRef(null);
+
+  useEffect(() => {
+    const steps = sliderRef.current.querySelectorAll('.slider__steps span');
+    steps.forEach((step, index) => {
+      if (index === value) {
+        step.classList.add('slider__step-active');
+      } else {
+        step.classList.remove('slider__step-active');
+      }
+    });
+  }, [value]);
+
+  const handleThumbDrag = (e) => {
+    const thumb = sliderRef.current.querySelector('.slider__thumb');
+    const progress = sliderRef.current.querySelector('.slider__progress');
+    const steps = sliderRef.current.querySelectorAll('.slider__steps span');
+    const sliderRect = sliderRef.current.getBoundingClientRect();
+    const offsetX = e.clientX - sliderRect.left;
+
+    let newValue = Math.round(((config.steps - 1) * offsetX) / sliderRect.width);
+
+    if (newValue < 0) {
+      newValue = 0;
+    } else if (newValue > config.steps) {
+      newValue = config.steps - 1;
+    }
+
+    setValue(newValue);
+
+    thumb.style.left = `${(newValue / (config.steps - 1)) * 100}%`;
+    progress.style.width = `${(newValue / (config.steps - 1)) * 100}%`;
+
+    steps.forEach((step, index) => {
+      if (index === newValue) {
+        step.classList.add('slider__step-active');
+      } else {
+        step.classList.remove('slider__step-active');
+      }
+    });
+  };
+
+  return (
+    <div
+      className="slider"
+      ref={sliderRef}
+      onMouseDown={handleThumbDrag}
+      onTouchStart={handleThumbDrag}
+    >
+      <div className="slider__thumb" style={{ left: `${(value / (config.steps - 1)) * 100}%` }}>
+        <span className="slider__value">{value}</span>
+      </div>
+      <div className="slider__progress" style={{ width: `${(value / (config.steps - 1)) * 100}%` }}></div>
+      <div className="slider__steps">
+        {Array.from({ length: config.steps }).map((_, index) => (
+          <span key={index}></span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default StepSlider;
