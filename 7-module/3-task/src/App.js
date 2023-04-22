6cb5b6c39b2f417011b3d3d0b8af9fb549a8b349
P4eJIa <p4ejia@gmail.com>
2023-04-22 15:21:02 +1000

    в процессе

--- 7-module/3-task/src/App.js
+++ 7-module/3-task/src/App.js
@@ -1,21 +1,71 @@
+import { useState } from "react";
 
 function App() {
+
+  let config = {
+    steps: 5,
+    value: 0
+  }
+
+  const [activeStep, setActiveStep] = useState('');
+  const element = document.querySelector('.slider');
+
+  const activeSlider = ((evt, i) => {
+    evt.preventDefault();
+    let left = evt.clientX - element.getBoundingClientRect().left;
+    let leftRelative = left / element.offsetWidth;
+    let segments = config.steps - 1;
+    let value = Math.round(leftRelative * segments);
+    let valuePercents = value / segments * 100;
+    let thumb = element.querySelector('.slider__thumb');
+    let progress = element.querySelector('.slider__progress');
+    element.querySelector('.slider__value').innerHTML = value;
+    thumb.style.left = (value === 0 ? 0 : `${valuePercents}%`);
+    progress.style.width = (value === 0 ? 0 : `${valuePercents}%`)
+    const sliderStep = evt.target.closest('slider__step');
+    setActiveStep(i);
+    const prevActiveStep = document.querySelector('.slider__step-active');
+    if (prevActiveStep) {
+      prevActiveStep.classList.remove('slider__step-active');
+    }
+    if (sliderStep) {
+      sliderStep.classList.add('slider__step-active')
+    }
+    const slideSelectEvent = new CustomEvent('slider-change', {
+      detail: config.value,
+      bubbles: true
+    });
+    document.documentElement.dispatchEvent(slideSelectEvent);
+  })
+
+  const getSliderSpan = config => {
+    let spanHTML = [];
+    for (let i = 0; i < config.steps; i++) {
+      spanHTML.push(
+        <span 
+          className={`${i === activeStep ? 'slider__step-active' : ''}`}
+          key={i}
+          onClick={(evt) => activeSlider(evt, i)}
+        >
+          {i}
+        </span>
+      )
+    }
+    return spanHTML;
+  }
+
   return (
     <div className="container" style={{padding: '50px'}}>
       <div className="slider">
 
-        <div className="slider__thumb" style={{left: '50%'}}>
-          <span className="slider__value">2</span>
+        <div className="slider__thumb" style={{left: '{(100 / (config.steps - 1)) * config.value}'}}>
+          <span className="slider__value"></span>
         </div>
 
-        <div className="slider__progress" style={{width: '50%'}}></div>
+        <div className="slider__progress" style={{width: '{(100 / (config.steps - 1)) * config.value}'}}></div>
 
         <div className="slider__steps">
-          <span></span>
-          <span></span>
-          <span className="slider__step-active"></span>
-          <span></span>
-          <span></span>
+          {getSliderSpan(config)}
         </div>
       </div>
 
