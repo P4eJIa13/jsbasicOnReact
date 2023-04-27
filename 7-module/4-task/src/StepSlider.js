d75628882cf28f6860efd2432c9496bf7040f423
P4eJIa <p4ejia@gmail.com>
2023-04-27 11:54:16 +1000

    Решение 4-task 7-module

--- 7-module/4-task/src/StepSlider.js
+++ 7-module/4-task/src/StepSlider.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect, useRef } from 'react';
 
 function StepSlider({ config }) {
   const [value, setValue] = useState(config.value);
+  const [dragging, setDragging] = useState(false);
   const sliderRef = useRef(null);
+  const thumbRef = useRef(null);
 
   useEffect(() => {
-    const steps = sliderRef.current.querySelectorAll('.slider__steps span');
-    steps.forEach((step, index) => {
+    const sliderSteps = sliderRef.current.querySelectorAll('.slider__steps span');
+    sliderSteps.forEach((step, index) => {
       if (index === value) {
         step.classList.add('slider__step-active');
       } else {
@@ -21,10 +23,21 @@ function StepSlider({ config }) {
     sliderRef.current.dispatchEvent(sliderChangeEvent);
   }, [value]);
 
-  const handleThumbDrag = (event) => {
-    const thumb = sliderRef.current.querySelector('.slider__thumb');
+  const onThumbPointerDown = (event) => {
+    setDragging(true);
+    sliderRef.current.classList.add('slider_dragging');
+  }
+
+  const onThumbPointerUp = (event) => {
+    setDragging(false);
+    sliderRef.current.classList.remove('slider_dragging');
+  }
+
+  const onThumbPointerMove = (event) => {
+    if (!dragging) return;
+    const thumb = thumbRef.current;
     const progress = sliderRef.current.querySelector('.slider__progress');
-    const steps = sliderRef.current.querySelectorAll('.slider__steps span');
+    const sliderSteps = sliderRef.current.querySelectorAll('.slider__steps span');
     const sliderRect = sliderRef.current.getBoundingClientRect();
     const offsetX = event.clientX - sliderRect.left;
 
@@ -32,7 +45,7 @@ function StepSlider({ config }) {
 
     if (newValue < 0) {
       newValue = 0;
-    } else if (newValue > config.steps) {
+    } else if (newValue > config.steps - 1) {
       newValue = config.steps - 1;
     }
 
@@ -41,7 +54,7 @@ function StepSlider({ config }) {
     thumb.style.left = `${(newValue / (config.steps - 1)) * 100}%`;
     progress.style.width = `${(newValue / (config.steps - 1)) * 100}%`;
 
-    steps.forEach((step, index) => {
+    sliderSteps.forEach((step, index) => {
       if (index === newValue) {
         step.classList.add('slider__step-active');
       } else {
@@ -50,14 +63,47 @@ function StepSlider({ config }) {
     });
   };
 
+  const onSliderClick = (event) => {
+    const sliderRect = sliderRef.current.getBoundingClientRect();
+    const offsetX = event.clientX - sliderRect.left;
+    let newValue = Math.round(((config.steps - 1) * offsetX) / sliderRect.width)
+    if (newValue < 0) {
+      newValue = 0;
+    } else if (newValue > config.steps - 1) {
+      newValue = config.steps - 1;
+    }
+    setValue(newValue);
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousemove', onThumbPointerMove);
+    document.addEventListener('mouseup', onThumbPointerUp);
+    document.addEventListener('touchmove', onThumbPointerMove);
+    document.addEventListener('touchend', onThumbPointerUp);
+
+    return () => {
+      document.removeEventListener('mousemove', onThumbPointerMove);
+      document.removeEventListener('mouseup', onThumbPointerUp);
+      document.removeEventListener('touchmove', onThumbPointerMove);
+      document.removeEventListener('touchend', onThumbPointerUp);
+    }
+
+    // eslint-disable-next-line
+  }, [dragging])
+
   return (
     <div
       className="slider"
       ref={sliderRef}
-      onMouseDown={handleThumbDrag}
-      onTouchStart={handleThumbDrag}
+      onMouseDown={onThumbPointerDown}
+      onTouchStart={onThumbPointerDown}
+      onClick={onSliderClick}
     >
-      <div className="slider__thumb" style={{ left: `${(value / (config.steps - 1)) * 100}%` }}>
+      <div 
+        className="slider__thumb" 
+        style={{ left: `${(value / (config.steps - 1)) * 100}%` }}
+        ref={thumbRef}
+      >
         <span className="slider__value">{value}</span>
       </div>
       <div className="slider__progress" style={{ width: `${(value / (config.steps - 1)) * 100}%` }}></div>
